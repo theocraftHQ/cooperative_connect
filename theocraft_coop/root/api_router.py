8cd1f2bbfea412216ce8394c1c3d6912c3ffb00c
law101 <lawrencebolu@gmail.com>
2025-08-22 19:45:56 -0300

    Added routers fro coop management

--- theocraft_coop/root/api_router.py
+++ theocraft_coop/root/api_router.py
@@ -3,9 +3,14 @@ from fastapi import APIRouter
 from theocraft_coop.routers.auth_route import api_router as auth_router
 from theocraft_coop.routers.misc_route import api_router as misc_router
 from theocraft_coop.routers.coop_auth_route import api_router as coop_auth_router
+from theocraft_coop.routers.coop_member_route import api_router as coop_member_router
+from theocraft_coop.routers.coop_route import api_router as coop_router
 
 router = APIRouter()
 
 router.include_router(router=auth_router)
 router.include_router(router=misc_router)
 router.include_router(router=coop_auth_router)
+router.include_router(router=coop_router)
+router.include_router(router=coop_member_router)
+
